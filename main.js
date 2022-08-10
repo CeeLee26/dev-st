f38cfff54f9585237d8278187664cf3060c63a44
ChinwE <105245392+CeeLee-e@users.noreply.github.com>
2022-08-09 20:41:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function updateSubarr(arr, step = 0) {
-  arr.forEach((char, i) => (arr[i] += step));
+  arr.forEach((char, i) => arr[i] += step);
   return arr;
 }
-console.log(updateSubarr([4, 14, 67], 3));
 
-/*P: Function inputs are an array plus default parament variable.
+const updateArr = updateSubarr([4, 14, 67], 3);
+console.log(updateArr);
+
+/*P: Function inputs are an array plus default parament, variable.
   R: Function returns an array.
   E: Each subarray is updated by the step variable value.
   P: Function loops through each subarray and reassigns their value 
-     by the concatation of the variable. When the loop ends, the changed 
+     by the concatenation of the variable. When the loop ends, the changed 
      subarrays populate the original array.*/
