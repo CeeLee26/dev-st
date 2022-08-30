a8b1d9692ece5b485e9a33113d02ec131039135e
ChinwE <105245392+CeeLee26@users.noreply.github.com>
2022-08-29 21:45:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -138,32 +138,12 @@ variable1 == true && variable2 == false;
 variable1 > variable2;
 variable1 && !variable2;
 
+n & 1;
+n % 2;
+n % 2 == 1;
 ____________________________________________________________
 
-Codewars'
-// Using if...else, explicit block syntax
-function solution(a, b) {
-    if (a.length < b.length) {
-        return a + b + a;
-    } else {
-        return b + a + b;
-    }
-}
 
-// Using if..., implicit else, compressed syntax
-function solution(a, b) {
-    if (a.length < b.length) return a + b + a;
-    return b + a + b;
-}
-
-// Using ternary conditional operator
-function solution(a, b) {
-    return (a.length < b.length) ? (a + b + a) : (b + a + b);
-}
-
-// ES6 arrow function
-var solution = (a, b) =>
-    (a.length < b.length) ? (a + b + a) : (b + a + b);
 
 ____________________________________________________________
 
