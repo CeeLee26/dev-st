830a013fa973ac000440a5cd001b7f1034ad172f
ChinwE <105245392+CeeLee26@users.noreply.github.com>
2022-10-11 23:37:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ const updateArr = updateSubarr([4, 14, 67], 3);
 //updateArr will be logged (via log method) to the console object.
 console.log(updateArr); //[7, 17, 70]
 
-/*P: Function inputs are an array plus default 
+/* Function inputs are an array plus default 
      parament, variable.
-  R: Function returns an array.
-  E: Each subarray is updated by the step 
+   Function returns an array.
+   Each subarray is updated by the step 
      variable's value.
-  P: Function loops through each subarray and
+   Function loops through each subarray and
      reassigns their value by the 
      concatenation of the variable. When the 
      loop ends, the changed subarrays populate 
@@ -211,7 +211,7 @@ ____
 arr.sort(...).pop()
 ...
 
-new Set[...arr].delete(subarray)
+new Set[...arr].delete(subarray) //u
 delete subarray (plus flat()) or prop Variable
 .splice(i, amt)
 ...
